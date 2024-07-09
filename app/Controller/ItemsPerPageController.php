9295d4a657bbc8683dc4bf029cd7fdb7c51f0e59
Liudvikas <liudvikas@galvotas.lt>
2024-07-09 10:58:11 +0300

    change redirect to page (items per page function)

--- app/Controller/ItemsPerPageController.php
+++ app/Controller/ItemsPerPageController.php
@@ -9,13 +9,25 @@ class ItemsPerPageController extends BaseController
 {
     public function setItemsPerPage()
     {
+        $task = $this->getTask();
         $limit = $this->request->getIntegerParam('max', 15);
         $this->userSession->setDashboardMaxItemsPerPage($limit);
-        
-        // Get the URL parameter
-        $url = $this->request->getStringParam('url', '');
-        
-        // Redirect to the saved URL
-        $this->response->redirect($url);
+                
+        $this->redirectAfterItemsPerPageChange($task);
+    }
+
+    protected function redirectAfterItemsPerPageChange(array $task)
+    {
+        switch ($this->request->getStringParam('redirect')) {
+            case 'list':
+                $this->response->redirect($this->helper->url->to('TaskListController', 'show', ['project_id' => $task['project_id']]));
+                break;
+            case 'dashboard':
+                $this->response->redirect($this->helper->url->to('DashboardController', 'show', [], 'project-tasks-'.$task['project_id']));
+                break;
+            case 'dashboard-tasks':
+                $this->response->redirect($this->helper->url->to('DashboardController', 'tasks', ['user_id' => $this->userSession->getId()]));
+                break;
+        }
     }
 }
--- app/Template/task_list/item_amount_list.php
+++ app/Template/task_list/item_amount_list.php
@@ -1,21 +1,16 @@
-<?php
-// Save the current URL
-$currentUrl = $_SERVER['REQUEST_URI'];
-?>
-
 <div class="dropdown">
     <!-- Items per page dropdown -->
     <a href="#" class="dropdown-menu dropdown-menu-link-icon">
         <strong><?= t('Items per page') ?> <i class="fa fa-caret-down"></i></strong>
     </a>
     <ul>
-        <li><?= $this->url->link('5', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 5, 'url' => $currentUrl)) ?></li>
-        <li><?= $this->url->link('10', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 10, 'url' => $currentUrl)) ?></li>
-        <li><?= $this->url->link('15', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 15, 'url' => $currentUrl)) ?></li>
-        <li><?= $this->url->link('20', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 20, 'url' => $currentUrl)) ?></li>
-        <li><?= $this->url->link('30', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 30, 'url' => $currentUrl)) ?></li>
-        <li><?= $this->url->link('40', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 40, 'url' => $currentUrl)) ?></li>
-        <li><?= $this->url->link('50', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 50, 'url' => $currentUrl)) ?></li>
+        <li><?= $this->url->link('5', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 5,)) ?></li>
+        <li><?= $this->url->link('10', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 10)) ?></li>
+        <li><?= $this->url->link('15', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 15)) ?></li>
+        <li><?= $this->url->link('20', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 20)) ?></li>
+        <li><?= $this->url->link('30', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 30)) ?></li>
+        <li><?= $this->url->link('40', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 40)) ?></li>
+        <li><?= $this->url->link('50', 'ItemsPerPageController', 'setItemsPerPage', array('max' => 50)) ?></li>
         <!-- Add more options as needed -->
     </ul>
 </div>
